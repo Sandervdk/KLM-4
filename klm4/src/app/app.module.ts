b1bffd0b25052ba505418410b4b5b76f340d6d2d
4libutt <ali-butt@live.nl>
2019-10-06 15:13:54 +0200

    added a header, nav-bar and footer component

--- klm4/src/app/app.module.ts
+++ klm4/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/mainpage/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
