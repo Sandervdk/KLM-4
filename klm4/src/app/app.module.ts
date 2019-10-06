3e3a1f453602262bbc9c4eeff6994ea8d57116fc
4libutt <ali-butt@live.nl>
2019-10-06 16:36:53 +0200

    added home & page-not-found components
    shuffled the controllers around and added a few paths to the routing

--- klm4/src/app/app.module.ts
+++ klm4/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/mainpage/header/header.component';
-import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/mainpage/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/mainpage/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
