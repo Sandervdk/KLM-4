65761a797adf7013a7ff0cbd3821f21cef20b545
Sander van de Kamp <sander.van.de.kamp@hva.nl>
2019-11-19 23:05:38 +0100

    Moved navbar to a single component, added it to the root html file

--- klm4/src/app/components/nav-bar/nav-bar.component.ts
+++ klm4/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Route, Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
-import {Functions} from '../../models/staff/Functions';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,53 +8,55 @@ import {Functions} from '../../models/staff/Functions';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  mechanicMode = false;
-  runnerMode = false;
-  adminMode = false;
+  currentRoute: String[];
+  notLoginPage: boolean;
 
   constructor(private router: Router, private route: ActivatedRoute, private authentication: AuthenticationService) {
+    this.router.events.subscribe(() => {
+      //disables the navbar for the login screen
+      this.notLoginPage = this.router.url !== '/signin';
+
+      //initializes the array and stets the first dash index to 1 so it skips the dash at the start
+      this.currentRoute = [];
+      let lastDash: number = 1;
+      let tempString: String;
+
+      //loops through the url length, checking for every dash and adds a seperate item in the currenRoute array for
+      //each string between two dashes. If the loop has reached the end, it will also add the last string to the array
+      for (let i = 1; i < this.router.url.length; i++) {
+        if (this.router.url.charAt(i) === '/') {
+          tempString = this.router.url.substring(lastDash, i);
+          tempString = this.fixLink(tempString);
+          this.currentRoute.push(tempString);
+          lastDash = i + 1;
+        } else if (i === this.router.url.length - 1) {
+          tempString = this.router.url.substring(lastDash);
+          tempString = this.fixLink(tempString);
+          this.currentRoute.push(tempString);
+        }
+      }
+    })
   }
 
-  ngOnInit() {
-    const userRole = this.authentication.getUser().getRole();
-    if (userRole === Functions.RUNNER) {
-      this.showRunner();
-    } else if (userRole === Functions.MECHANIC) {
-      this.showMechanic();
-    } else if (userRole === Functions.ADMIN) {
-      this.showAdmin();
+  ngOnInit() {}
+
+  //Reformats the links to make sure the display names start with an upper case letter and the rest is lower case.
+  private fixLink(string: String): String {
+    return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+  }
+
+  //takes the index(+1) of the clicked link and creates a valid routing string based on how 'deep' the link is
+  //in case the second link gets clicked, it will have to take the first and second part of the location array.
+  navigate(index: number) {
+    let location = "";
+    for (let i = 0; i < index; i++) {
+      location += "/" + this.currentRoute[i];
     }
-  }
-
-  showAdmin() {
-    this.adminMode = true;
-    this.runnerMode = false;
-    this.mechanicMode = false;
-    this.router.navigate(['/admin'], {
-      relativeTo: this.route
-    });
-  }
-
-  showRunner() {
-    this.runnerMode = true;
-    this.mechanicMode = false;
-    this.router.navigate(['/runner'], {
-      relativeTo: this.route
-    });
-  }
-
-  showMechanic() {
-    this.mechanicMode = true;
-    this.runnerMode = false;
-    this.router.navigate(['/mechanic'], {
-      relativeTo: this.route
-    });
+    this.router.navigate([location.toLowerCase()]);
   }
 
   logOut() {
-    this.mechanicMode = false;
-    this.runnerMode = false;
-    this.adminMode = false;
     this.authentication.signOut();
+    this.router.navigate(['/signin']);
   }
 }
