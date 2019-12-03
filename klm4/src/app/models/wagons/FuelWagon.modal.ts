cb0bbdaf8eff8eef2575ecf830f28f22f76f745b
Sander van de Kamp <sander.van.de.kamp@hva.nl>
2019-12-03 17:44:11 +0100

    fFixed fuelWagons

--- klm4/src/app/models/wagons/FuelWagon.modal.ts
+++ klm4/src/app/models/wagons/FuelWagon.modal.ts
@@ -1,8 +1,10 @@
 import {Wagon} from './Wagon.modal';
 import {WagonTypes} from '../enums/wagonTypes';
+import {EquipmentStatus} from "../enums/equipmentStatus";
+import {LatLng} from "leaflet";
 
 export class FuelWagon extends Wagon {
-  constructor(id: number, title: string, lastSeen: {}) {
-    super(id, title, lastSeen, WagonTypes.STIKSTOFWAGEN);
+  constructor(id: number, title: string, lastSeen: LatLng) {
+    super(id, title, lastSeen, WagonTypes.STIKSTOFWAGEN, EquipmentStatus.AVAILABLE);
   }
 }
