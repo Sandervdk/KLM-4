e278454f18218f3e11abf976e8c11c199fcd1c20
Sagi Lalee <Sagi_lalee@hotmail.com>
2019-12-09 15:18:57 +0100

    sql data id value change

--- klm4/spring-boot-backend/src/main/resources/data.sql
+++ klm4/spring-boot-backend/src/main/resources/data.sql
@@ -1,5 +1,8 @@
-INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (1, 'mechanic@klm.nl', 'Welkom01', 'Mechanic', 'mac', 1);
-INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (2,'admin@klm.nl', 'Welkom01', 'admin', 'admin', 0);
-INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (3,'runner@klm.nl', 'Welkom01', 'runner', 'runner', 2);
+INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (1001, 'mechanic@klm.nl', 'Welkom01', 'Mechanic', 'mac', 1);
+INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (1002,'admin@klm.nl', 'Welkom01', 'admin', 'admin', 0);
+INSERT INTO USER(id, email, password, firstname, lastname, role) VALUES (1003,'runner@klm.nl', 'Welkom01', 'runner', 'runner', 2);
 
-INSERT INTO MELDING(id, locatie, deadline, wagon_types, type_vliegtuig, positie, tijd, status) VALUES (1000,'F5', '14:00:33',1,2,'G5', '14:00:33',1)
+INSERT INTO MELDING(id, locatie, deadline, wagon_types, type_vliegtuig, positie, tijd, status) VALUES (1000,'F5', '14:00:33',1,2,'G5', '14:00:33',1);
+INSERT INTO MELDING(id, locatie, deadline, wagon_types, type_vliegtuig, positie, tijd, status) VALUES (1001,'F5', '14:00:33',1,2,'G5', '14:00:33',1);
+INSERT INTO MELDING(id, locatie, deadline, wagon_types, type_vliegtuig, positie, tijd, status) VALUES (1002,'F5', '14:00:33',1,2,'G5', '14:00:33',1);
+INSERT INTO MELDING(id, locatie, deadline, wagon_types, type_vliegtuig, positie, tijd, status) VALUES (1003,'F5', '14:00:33',1,2,'G5', '14:00:33',1);
