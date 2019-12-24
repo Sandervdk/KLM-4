b5bd20a4a6f59860ff36b5f51d551dc4e7201586
Sagi Lalee <Sagi_lalee@hotmail.com>
2019-12-24 15:43:39 +0100

    allowed headers in APIconfig

--- klm4/spring-boot-backend/src/main/java/com/example/demo/config/APIconfig.java
+++ klm4/spring-boot-backend/src/main/java/com/example/demo/config/APIconfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,9 @@ public class APIconfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
+      .allowCredentials(true)
+      .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+      .exposedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
       .allowedMethods("GET", "POST", "PUT", "DELETE")
       .exposedHeaders("Authorization")
       .allowedOrigins("*");
